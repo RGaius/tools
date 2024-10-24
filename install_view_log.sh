e4010ff9289e58cc8dd6ede2246a92938ddb9090
gaius.zhao <gaius.zhao@yunzhixun.com>
2024-10-24 11:32:28 +0800

    Add installation script

---
+++ install_view_log.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 下载脚本
+wget https://raw.githubusercontent.com/rgaius/tools/main/view_log.sh -O /usr/local/bin/view_log
+
+# 赋予执行权限
+chmod +x /usr/local/bin/view_log
+
+echo "Log viewer script installed successfully."
